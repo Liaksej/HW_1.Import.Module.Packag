d6c16c85f54be9faace7e5a2bfa8a6ccb55c0eae
Liaksej <tarusov.liaksej@gmail.com>
2022-08-28 18:19:44 +0200

    Initial commit

---
+++ application/spotify/spotify.py
@@ -0,0 +1,6 @@
+import spotify_backup
+
+if __name__ == '__main__':
+    # Не получилось пока разобраться, как запускать его.
+    exec('spotify_backup mybackup.csv --dump playlists,liked --token')
+
