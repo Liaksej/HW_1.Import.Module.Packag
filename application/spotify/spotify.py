04f847b078fddbd958341f2b8fea8b74bf20f786
Liaksej <tarusov.liaksej@gmail.com>
2022-08-28 18:22:31 +0200

    Improve description

--- application/spotify/spotify.py
+++ application/spotify/spotify.py
@@ -1,6 +1,6 @@
 import spotify_backup
 
 if __name__ == '__main__':
-    # Не получилось пока разобраться, как запускать его.
+    # Не получилось пока разобраться, как запускать его spotify_backup.
     exec('spotify_backup mybackup.csv --dump playlists,liked --token')
 
